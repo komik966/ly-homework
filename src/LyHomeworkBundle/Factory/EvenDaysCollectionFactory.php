42db9ff37d7dbb55e8f287dd4316b6418b69569a
konradmikucki <komik966@gmail.com>
2017-11-07 22:29:56 +0100

    collection & command class skeletons

---
+++ src/LyHomeworkBundle/Factory/EvenDaysCollectionFactory.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace LyHomeworkBundle\Factory;
+
+class EvenDaysCollectionFactory
+{
+    public function create(string $format = "d-m-Y", int $year = 2021): array
+    {
+        return [];
+    }
+}
---
+++ tests/LyHomeworkBundle/Integration/Command/EvenDaysCommandTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tests\LyHomeworkBundle\Integration\Command;
+
+use LyHomeworkBundle\Command\EvenDaysCommand;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EvenDaysCommandTest extends WebTestCase
+{
+
+}
